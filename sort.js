ea4601c845ff7fb9e750c98a4256ce9e7a0b3c6a
Wis-ssam <96730184+Wis-ssam@users.noreply.github.com>
2022-03-11 10:23:55 +0100

    Add files via upload

---
+++ sort.js
@@ -0,0 +1,21 @@
+const arr =[ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+const ar =[ 20, 100, -100, -3, 5, 0, -2 ];
+
+// output -4,-3,1,2,3,5,6,7,8
+    let temp;
+function tri(vecteur){
+    for(var i=0 ; i <vecteur.length - 1; i++){
+        for(var j=i+1 ; j<vecteur.length ; j++){
+            if(vecteur[j]<vecteur[i]){
+                temp=vecteur[j];
+                vecteur[j]=vecteur[i];
+                vecteur[i]=temp;
+            }
+        }
+    }
+        
+    return vecteur
+}
+    
+console.log(tri(arr));
+console.log(tri(ar));
